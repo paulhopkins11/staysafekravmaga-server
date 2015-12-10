2cbf1f7bc87f1282b292759d597a1b2ec1833615
Paul Hopkins <paul.hopkins@os.uk>
2015-12-10 15:46:52 +0000

    Initial Creation

---
+++ run-server.sh
@@ -0,0 +1,1 @@
+docker run -p 90:80 -it -v "$PWD"/src:/var/www/html php:5.6-apache
---
+++ src/test.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+GOT SOMETHING HERE
+<?php
+
+echo 'Hello ' . htmlspecialchars($_GET["firstname"]) . ' ' . htmlspecialchars($_GET["surname"]);
+?>
+</body>
+</html>
